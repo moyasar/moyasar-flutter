432a47816a154679f6b187ebfe607219e8a7a808
Faisal Alghurayri <fghurayri@gmail.com>
2024-08-12 15:12:43 -0400

    Add supportedCountries in Apple Pay config

--- lib/src/models/apple_pay_config.dart
+++ lib/src/models/apple_pay_config.dart
@@ -12,11 +12,17 @@ class ApplePayConfig {
   /// Payment capabilities that the merchant supports.
   List<String> merchantCapabilities;
 
+  /// A list of ISO 3166 country codes to limit payments to cards from set countries.
+  /// Only Saudi Arabia is enabled by default to prevent fraudulent transactions.
+  /// You may enable more countries at your own risk.
+  List<String> supportedCountries;
+
   ApplePayConfig(
       {required this.merchantId,
       required this.label,
       required this.manual,
-      this.merchantCapabilities = const ["3DS", "debit", "credit"]})
+      this.merchantCapabilities = const ["3DS", "debit", "credit"],
+      this.supportedCountries = const ["SA"]})
       : assert(merchantCapabilities.contains("3DS"),
             "Merchant capabilities must contain 3DS.");
 }
